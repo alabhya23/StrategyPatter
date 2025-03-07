c16bd5b6f7e74b53519503d9b8d6f5043c31755a
alabhya23 <mishtysharma55@gmail.com>
2025-03-07 09:39:28 +0530

    Duck.java

---
+++ Duck.java
@@ -0,0 +1,27 @@
+public abstract class Duck {
+    QuackBehaviour quackBehaviour;
+    SwimBehaviour swimBehaviour;
+    FlyBehaviour flyBehaviour;
+
+
+    abstract public void display();
+
+    public void setQuackBehaviour(QuackBehaviour qb) {
+        quackBehaviour = qb;
+    }
+    public void performQuack() {
+        quackBehaviour.quack();
+    }
+    public void setSwimBehaviour(SwimBehaviour sb) {
+        swimBehaviour = sb;
+    }
+    public void performSwim() {
+        swimBehaviour.swim();
+    }
+    public void setFlyBehaviour(FlyBehaviour fb) {
+        flyBehaviour = fb;
+    }
+    public void performFly() {
+        flyBehaviour.fly();
+    }
+}
